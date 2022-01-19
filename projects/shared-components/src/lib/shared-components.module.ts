5a17df3216eb1b677beeb4c462da68df5d84a69a
Victor Bittencourt <victorlbitten@gmail.com>
2022-01-19 13:37:43 -0300

    Removendo exemplos do Angular

--- projects/shared-components/src/lib/shared-components.module.ts
+++ projects/shared-components/src/lib/shared-components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-import { SharedComponentsComponent } from './shared-components.component';
 
 
 
 @NgModule({
   declarations: [
-    SharedComponentsComponent
   ],
+
   imports: [
   ],
+
   exports: [
-    SharedComponentsComponent
   ]
+
 })
+
+
+
 export class SharedComponentsModule { }
--- projects/shared-components/src/public-api.ts
+++ projects/shared-components/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of shared-components
  */
 
-export * from './lib/shared-components.service';
-export * from './lib/shared-components.component';
+
+
 export * from './lib/shared-components.module';
